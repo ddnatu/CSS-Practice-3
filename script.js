c640c173e14967ca34abb3b85e7f461f3a71839c
Devdutta Natu <natudevdutta@gmail.com>
2016-08-10 19:40:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var app = angular.module('myApp',[]);
+app.controller('myController',function($scope){
+	$scope.names = ['devdutta natu','mukul chakravarty','rupesh kumar singh','sarthak saxena','monisha nair','parul raut','sujeet jaiswal','sanchit saxena'];
+	price = 5;		
+})
